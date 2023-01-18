c024af7a925957161d7e0e4713265be1c92d5feb
chaitanya krishna Chirumamilla <ch.chaitanya94@gmail.com>
2023-01-18 16:30:33 +0100

    added imaged and midified loadDisplaySave file

---
+++ build/CMakeFiles/loadDisplaySave.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/chaitu/workspace/Opencv_ws/src/chapter1/loadDisplaySave.cpp" "/home/chaitu/workspace/Opencv_ws/build/CMakeFiles/loadDisplaySave.dir/src/chapter1/loadDisplaySave.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../images"
+  "/usr/local/include/opencv4"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ src/chapter1/loadDisplaySave.cpp
@@ -0,0 +1,40 @@
+ #include <opencv2/opencv.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core.hpp>
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+using namespace cv;
+
+
+
+int main()
+{
+    Mat image= cv::imread("../../images/puppy.bmp",IMREAD_GRAYSCALE); // read an input image
+	cout << "This image has " << image.channels() << " channel(s)";
+	if (image.empty()) { // error handling
+	// no image has been created...
+	// possibly display an error message
+	// and quit the application
+		cout<< "no image has been created..." << endl;
+	}
+
+	// define the window (optional)
+	cv::namedWindow("Original Image");
+	// show the image
+	cv::imshow("Original Image", image);
+	std::cout << "This image is " << image.rows << " x " << image.cols<< std::endl;
+
+	cv::Mat result; // we create
+	cv::flip(image,result,0);
+	//another empty image
+	//positive for horizontal
+	//0 for vertical,negative for both
+	cv::namedWindow("Output Image"); // the output window
+	cv::imshow("Output Image", result);
+	cv::imwrite("output.bmp", result); // save result
+	waitKey(0);
+    return 0;
+}
+
